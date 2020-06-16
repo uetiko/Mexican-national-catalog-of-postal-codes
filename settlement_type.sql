7f392d25ad3867a08542bd26395e634579892fef
Angel Barrientos <uetiko@gmail.com>
2020-06-16 00:32:18 -0500

    Added new general tables
    the zone and settlement type are fixed

---
+++ settlement_type.sql
@@ -0,0 +1,103 @@
+
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('9129da9b-4e3f-4a91-93fb-dd4571e57108'), 'Colonia');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('c914f743-1e8a-46e5-bb28-6b57ad0cdb7f'), 'Unidad habitacional');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('11e58a31-4d87-4d43-8983-e78f1e2d484e'), 'Fraccionamiento');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('122566e9-887d-4dfa-beff-f82410d5a871'), 'Zona industrial');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('c44ac4b2-0a86-41a8-86d2-9db78424ef28'), 'Residencial');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('76499a9b-df7c-4865-b1d2-99c0f2c3b278'), 'Barrio');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('43d776b9-7233-4eba-8104-92543656e965'), 'Zona comercial');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('f38486a9-a94d-49e1-8882-bc05669cefc8'), 'Parque industrial');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('1d6cb782-78fb-4f44-935e-96c93bba8d31'), 'Rancho');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('81cf6e82-a27e-48e3-af0d-a787b32241be'), 'Equipamiento');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('47195858-9ebf-4cce-996e-a9a9cc25a8ad'), 'Ejido');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('b5e9013d-497b-419e-838b-9865d56de8c4'), 'Gran usuario');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('f3740a38-51f2-4164-b077-0a8031a53b2c'), 'Ranchería');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('d8855eaf-f67b-4485-b6e4-8b0782af71b7'), 'Pueblo');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('7e098d3f-fd93-4ef4-b720-7a030a551f6c'), 'Hacienda');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('d1bb744d-7d5d-4cd8-a048-4ba93f645c0d'), 'Aeropuerto');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('f6a98335-1dbc-4262-81af-ae149d6b5283'), 'Condominio');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('1d866c56-8ec2-4fe5-adaf-8e8250ecd80b'), 'Zona militar');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('08334404-5750-40b7-b114-8abaae7f7525'), 'Puerto');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('baee4e21-750e-4d1d-a87e-54a4f13594ad'), 'Ampliación');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('bf375f43-c717-43e9-ba9d-b73a7287fad8'), 'Conjunto habitacional');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('e8e4f5b5-1368-44f4-b379-0c8a931bb254'), 'Congregación');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('0abcb6cd-b7a0-4ce9-a156-2166d7a1e79b'), 'Poblado comunal');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('0d450a7e-e1a0-47c5-9078-b8d3d70db070'), 'Villa');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('cd96e319-7e93-4aff-a39c-95f6ef4eac56'), 'Estación');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('d8a9af4d-a38d-43d3-8510-915cf8399e9a'), 'Paraje');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('093a42b6-0a41-4124-a529-489a400c6415'), 'Finca');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('bce47b22-1b62-4d55-bea5-f116c47aa39c'), 'Ingenio');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('f663ba05-48ed-411f-ac42-ef906809fbfb'), 'Zona federal');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('c1247c85-be33-476a-a932-1c44cce8f93c'), 'Granja');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('e082ad07-d980-411a-85b1-d1f8ac8a3ba9'), 'Club de golf');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('a8beb41d-2b33-4cd8-9ce9-6ed8f0d9b5ce'), 'Campamento');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('42d783b7-f10f-4c73-a6dd-4d63af76319c'), 'Exhacienda');
+            
+            insert into settlement_type(id, name)
+            values(UUID_TO_BIN('a08acbc7-c7a7-48d7-bac9-4c727c25a666'), 'Zona naval');
+            
---
+++ zone.sql
@@ -0,0 +1,10 @@
+
+            insert into zone(id, name)
+            values(UUID_TO_BIN('bf52d3ce-ccdb-4295-89f5-27d42e103677'), 'Urbano');
+            
+            insert into zone(id, name)
+            values(UUID_TO_BIN('a706b772-8bde-4812-9403-a0fb8f7b0dce'), 'Rural');
+            
+            insert into zone(id, name)
+            values(UUID_TO_BIN('dbb374c6-c9e4-44e9-ae4b-fc1e26aea6a9'), 'Semiurbano');
+            
